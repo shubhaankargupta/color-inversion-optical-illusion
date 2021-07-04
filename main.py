29eb60be21d64c5d4ef741c4577e493a7765319d
Shubhaankar Gupta <63454581+guptashubhankar06@users.noreply.github.com>
2021-07-04 23:26:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ def ellipse(x, y, offset):
     draw = ImageDraw.Draw(inverted)
     w1, h1 = inverted.size
 
+    widthofbar = h1//40
+
     step = 10
     for n in range(step, w1, step):
         for p in range(step, h1 - step, step):
             draw.point((n, p))
-
-    draw.line((x, y, x + offset, y), fill="yellow", width=40)
+    draw.ellipse((w1/2-5, h1/2-5, w1/2+5, h1/2+5), fill=(0, 0, 0))
+    draw.line((x, y, x + offset, y), fill="white", width=widthofbar)
     return inverted
 
 
-def make_gif():
+def make_gif(imgpath):
     frames = []
     image = Image.open(imgpath)
     imgGray = image.convert('L')
@@ -30,21 +32,17 @@ def make_gif():
         frames.append(ellipse(x, y, offset))
         x += 00
         y += 00
-
-
-
     step = 10
     for n in range(step, wbw, step):
         for x in range(step, hbw - step, step):
             draw2.point((n, x), fill="yellow")
-
     frames.append(imgGray)
-    
+
     duration = 240000/wbw
     frame_one = frames[0]
     frame_one.save("circle.gif", format="GIF", append_images=frames,
                    save_all=True, duration=duration, quality=70)
 
-
+imgpath = '/Users/guptashubhankar06/Desktop/nature-forest-trees-macro-wallpaper.jpeg'
 if __name__ == "__main__":
-    make_gif()
+    make_gif(imgpath)
