2abd8abf765881902aefc64a6614efc5e6fd6ab8
Shubhaankar Gupta <63454581+guptashubhankar06@users.noreply.github.com>
2021-07-11 23:26:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+#importing dependancies
 from PIL import Image, ImageOps, ImageDraw
+
 
 def make_gif(imgpath):
     frames = []
     image = Image.open(imgpath)
+    
+    #converting image to greyscale
     imgGray = image.convert('L')
     draw2 = ImageDraw.Draw(imgGray)
-
+    
     wbw, hbw = imgGray.size
     x = 0
     y = hbw/1.1
@@ -13,13 +17,14 @@ def make_gif(imgpath):
     for number in range(wbw//30):
         offset += 30
         image = Image.open(imgpath)
+        #inverting colors of image
         inverted = ImageOps.invert(image)
         draw = ImageDraw.Draw(inverted)
         w1, h1 = inverted.size
 
         widthofbar = h1 // 40
-
         step = 10
+        #adding texture for more effective illusion
         for n in range(step, w1, step):
             for p in range(step, h1 - step, step):
                 draw.point((n, p))
@@ -28,6 +33,7 @@ def make_gif(imgpath):
         frames.append(inverted)
         x += 00
         y += 00
+
     step = 10
     for n in range(step, wbw, step):
         for x in range(step, hbw - step, step):
@@ -39,6 +45,6 @@ def make_gif(imgpath):
     frame_one.save("circle.gif", format="GIF", append_images=frames,
                    save_all=True, duration=duration, quality=70)
 
-imgpath = 'imgpath'
+imgpath = 'imgpath' #enter image path
 
 make_gif(imgpath)
